873b74597f1d22f22cbe896357346285e8f4cb2c
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-05-30 19:50:31 -0300

    Atualizações de Controle e View do Personagem

---
+++ src/controller/PersonagemController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import model.Personagem;
+
+public class PersonagemController {
+	
+	private static EntityManagerFactory manager = 
+			Persistence.createEntityManagerFactory("MortalKombat"); 
+	
+	
+	public static void Cadastrar(Personagem obj) {
+		EntityManager entitymanager = manager.createEntityManager();
+		entitymanager.getTransaction().begin();
+		entitymanager.persist(obj);
+		entitymanager.getTransaction().commit();
+	}
+	public List<Personagem> Listar() {
+		EntityManager entitymanager = manager.createEntityManager();
+		
+		TypedQuery<Personagem> query = null;
+		
+		query = entitymanager.createQuery("select c from Personagem c", Personagem.class);
+		
+		return query.getResultList();
+	}
+}
